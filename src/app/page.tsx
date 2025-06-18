d8bf1559ac6cd652362792278ab9e5cdb98fa234
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-18 15:00:12 -0300

    style: Tipografia configurada.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/app/components/button";
 export default function Home() {
   return (
     <main>
-      <div className="text-gray-100">Hello world!</div>
+      <div className="text-white">Hello world!</div>
+      <h1 className="text-4xl font-semibold font-heading">Hello world!</h1>
       <Button text="Diego" />
       <Button text="NLW" />
       <Button />
