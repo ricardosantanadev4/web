23736b410892deb9365875281e3fb87023ef4cb5
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-18 12:08:17 -0300

    styke: Variáveis css, definidas.

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,4 +1,7 @@
+interface ButtonProps {
+    text?: string;
+}
 
-export function Button() {
-    return (<button className="bg-violet-500 px-5 py-2 rounded-sm">Oi</button>)
+export function Button(props: ButtonProps) {
+    return (<button className="bg-violet-500 px-5 py-2 rounded-sm">{props.text || 'Enviar'}</button>)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/app/components/button";
 export default function Home() {
   return (
     <main>
-      <div className="text-white">Hello world!</div>
+      <div className="text-gray-100">Hello world!</div>
+      <Button text="Diego" />
+      <Button text="NLW" />
       <Button />
     </main>
   );
