e9f0531398294e8c26b30587426de29661435f15
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-19 17:51:06 -0300

    feat: Component de input e estado configurado.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 
-import { ArrowRight, Copy } from 'lucide-react';
+import { ArrowRight, Copy, Mail } from 'lucide-react';
 import { Button } from "./components/button";
 import { IconButton } from "./components/icon-button";
+import { Icon, InputField, InputRoot } from './components/input';
 
 export default function Home() {
   return (
@@ -14,6 +15,16 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot error>
+          <Icon>
+            <Mail className='size-5' />
+          </Icon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
