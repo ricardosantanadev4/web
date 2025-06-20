bd50e2c0a9b5ca33784c457d28d137deb92709b9
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-20 16:50:05 -0300

    refactor: AJuste na identação do código.

--- src/app/components/icon-button.tsx
+++ src/app/components/icon-button.tsx
@@ -5,7 +5,7 @@ interface IconButtonProps extends ComponentProps<'button'> { }
 export function IconButton(props: IconButtonProps) {
     return (
         <button
-            className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+            className='p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300'
             {...props}
         />
     )
--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from "react";
+import { ComponentProps } from "react"
 
 interface InputRootProps extends ComponentProps<'div'> {
     error?: boolean,
@@ -8,7 +8,7 @@ export function InputRoot({ error = false, ...props }: InputRootProps) {
     return (
         <div
             data-error={error}
-            className="group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger"
+            className='group bg-gray-800 h-12 border border-gray-600 rounded-xl px-4 flex items-center gap-2 focus-within:border-gray-100 data-[error=true]:border-danger'
             {...props}
         />
     )
@@ -19,7 +19,7 @@ interface IconProps extends ComponentProps<'span'> { }
 export function InputICon(props: IconProps) {
     return (
         <span
-            className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
+            className='text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger'
             {...props}
         />
     )
@@ -30,7 +30,7 @@ interface InputFieldProps extends ComponentProps<'input'> { }
 export function InputField(props: InputFieldProps) {
     return (
         <input
-            className="flex-1 outline-0 placeholder:gray-400"
+            className='flex-1 outline-0 placeholder:gray-400'
             {...props}
         />
     )
